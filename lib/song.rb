ec550953db5d677abda78c32ec4937af016e5f41
Barbara Washington <barbara.m.washington@gmail.com>
2018-05-16 19:08:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,9 @@ end
 
 def self.new_from_filename(filename)
   split_filename = filename.chop.chop.chop.chop.split(" - ")
-
+  song = Song.new
+  song.artist_name = split_filename[0]
+  song.name = split_filename[1]
 end
 
 end
